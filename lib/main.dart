7c41f13dfd767ededea4582104772657f8dc40d7
puchochiri <doawishfor@naver.com>
2023-07-19 22:51:20 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: // 여기에 예제 코드 작성하기
+        )
+      )
+    )
+  }
+}
